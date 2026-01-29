3bd5f311732bc451728d58ddfa115f8d37cb9ac1
Shaker <sshakiralmazini@gmail.com>
2026-01-29 22:22:45 +0200

    test code laravel

---
+++ resources/views/test.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Test pages </title>
+</head>
+<body>
+    <div class="container">
+        <h1 style="text-align: center; color:blue">Test Page</h1>
+        <p style="text-align: center; color:blueviolet">This is a test page for Laravel app setup.</p>
+    </div>
+
+    
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+Route::get('test', App\Http\Controllers\Test\TestController::class,"index");
+
 // Include health check routes
 require __DIR__.'/health.php';
